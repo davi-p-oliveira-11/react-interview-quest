4b87e7e68eaf66aa26b93bbb83a75360c58c5c19
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-13 14:47:10 -0300

    added reset button and functionality

--- src/components/props/Props.jsx
+++ src/components/props/Props.jsx
@@ -7,14 +7,21 @@ from parent to child components */}
 
 function Props() {
   const [name, setName ] = useState("John");
+
   const changeName = () => {
     setName('Kirk')
   }
+
+  const resetName = () => {
+    setName('John')
+  }
+
   return ( 
   <div className="Props">
     <Child valueOfProps={name} changeName={changeName}/>
 
     <button onClick={() => setName("Paul")}>Change Name</button>
+    <button onClick={resetName}>Reset Name</button>
   </div>
   )
 }
