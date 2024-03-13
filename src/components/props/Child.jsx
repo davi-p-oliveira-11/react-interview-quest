55d2143db0653ab365ff053ab9d05d3f52ac666e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-13 14:32:37 -0300

    added more logic

--- src/components/props/Child.jsx
+++ src/components/props/Child.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Child() {
+export default function Child({ valueOfProps, changeName }) {
   return (
-  <div>
-    <h1>Child</h1>
-  </div>
-  )
+    <div>
+      <h1>{valueOfProps}</h1>
+
+      <button onClick={() => changeName("Hello there")}>Change Name 2</button>
+    </div>
+  );
 }
--- src/components/props/Props.jsx
+++ src/components/props/Props.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react'
 import Child from './Child';
 import "./props.css"
 
+{/* Props ae used to transfer propeties
+from parent to child components */}
+
 function Props() {
   const [name, setName ] = useState("John");
-  return <div className="Props">
-    <Child/>
+  const changeName = () => {
+    setName('Kirk')
+  }
+  return ( 
+  <div className="Props">
+    <Child valueOfProps={name} changeName={changeName}/>
+
+    <button onClick={() => setName("Paul")}>Change Name</button>
   </div>
+  )
 }
 
 export default Props;
