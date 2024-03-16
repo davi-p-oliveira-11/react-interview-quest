13b18fe2739902f37220b2904dd3144c17efebee
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 02:16:28 -0300

    added keys logic

--- src/components/keys-react/Keys.jsx
+++ src/components/keys-react/Keys.jsx
@@ -1,6 +1,54 @@
 
 function Key() {
-  return <div className="Key"></div>
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const details = [
+    {
+      id: 1,
+      name: "Sasuke",
+      age: 21,
+    },
+    {
+      id: 2,
+      name: "Daniel",
+      age: 28,
+    },
+    {
+      id: 3,
+      name: "Daniel",
+      age: 28,
+    },
+    {
+      id: 4,
+      name: "Nicholas",
+      age: 20,
+    },
+    {
+      id: 5,
+      name: "Paul",
+      age: 31,
+    },
+  ];
+  return <div className="Key">
+    {/* 
+    <ul>
+      {numbers.map((number) => {
+        return <li key={number}>{number}</li>;
+      })}
+    </ul>
+    */}
+    
+
+    <ul>
+      {details.map((detail, index) => {
+        return (
+          <div key={index}>
+            <li>{detail.name}</li>
+            <li>{detail.age}</li>
+          </div>
+        )
+      })}
+    </ul>
+  </div>
 }
 
-export default Key
+export default Key;
