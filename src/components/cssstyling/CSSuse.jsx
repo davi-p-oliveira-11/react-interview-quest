2c7f3d63445b7305f506fcd236e06b3bff72b7c2
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 18:19:59 -0300

    deleted vite default css

--- src/components/cssstyling/CSSuse.jsx
+++ src/components/cssstyling/CSSuse.jsx
@@ -1,7 +1,9 @@
 import './css-use.css'
 
 function CSSuse(){
-  return <div className="css-use"></div>
+  return <div className="css-use">
+    <h1>Hello World</h1>
+  </div>
 }
 
 export default CSSuse;
