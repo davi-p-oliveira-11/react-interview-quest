75502e1ae7b1755877f70f2457e5dba306002a2e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 18:08:28 -0300

    added final logic to dynamic inputs

--- src/components/dinamicinputs/DynamicInputs.jsx
+++ src/components/dinamicinputs/DynamicInputs.jsx
@@ -1,16 +1,27 @@
-import './dyncamicinputs.css'
+import { useState } from "react";
+import "./dyncamicinputs.css";
 
 function DynamicInputs() {
+  const [inputs, setInputs] = useState({})
+  const getInputValues = (data) => {
+    let { name, value } = data.target;
+    let input = { [name] : value};
+    setInputs({ ...inputs, ...input });
+  };
   return (
-    <div className='dynamicinput'>
-      <div className='input-container'>
-        <input placeholder="Name" />
-        <input placeholder="Age" />
-        <input placeholder="Years of Experience" />
+    <div className="dynamicinput">
+      <div className="input-container">
+        <input placeholder="Name" name="name" onChange={getInputValues} />
+        <input placeholder="Age" name="age" onChange={getInputValues} />
+        <input
+          placeholder="Years of Experience"
+          name="ye"
+          onChange={getInputValues}
+        />
       </div>
       <button>Add New Group</button>
     </div>
-  )
+  );
 }
 
 export default DynamicInputs;
