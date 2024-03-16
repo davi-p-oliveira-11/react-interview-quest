ac550706658b539a793a22a55db0e69930a20625
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 01:37:52 -0300

    event handling component and logic

--- src/components/eventhandling/EventHandling.jsx
+++ src/components/eventhandling/EventHandling.jsx
@@ -1,6 +1,16 @@
-
 function EventHandler() {
-  return <div className="EventHandler"></div>
+  const getInput = (e) => {
+    console.log(e.target.name);
+  };
+  const addNums = (num1, num2) => {
+    console.log(num1 + num2);
+  };
+  return (
+    <div className="EventHandler">
+      <input placeholder="Add Something" onChange={getInput} name="input" />
+      <button onClick={() => addNums(2, 3)}>Add Numbers</button>
+    </div>
+  );
 }
 
-export default EventHandler
+export default EventHandler;
