08cc859c97e9ed1aa142a90b42806ebb3c9909fc
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 18:56:48 -0300

    added final logic to component

--- src/components/cssstyling/CSSuse.jsx
+++ src/components/cssstyling/CSSuse.jsx
@@ -1,9 +1,44 @@
-import './css-use.css'
+import React from "react";
+import "./css-use.css";
 
-function CSSuse(){
-  return <div className="css-use">
-    <h1>Hello World</h1>
-  </div>
+const styles = {
+  internal: {
+    color: "blue",
+  },
+  light: {
+    backgroundColor: "white",
+    color: "black",
+  },
+  dark: {
+    backgroundColor: "black",
+    color: "white",
+  },
+};
+
+function CSSuse() {
+  const [theme, setTheme] = React.useState(false);
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
+  return (
+    <div style={theme ? styles.light : styles.dark}>
+      <h1 className="external">External Styling</h1>
+
+      <h1>Internal Styling</h1>
+
+      <h1
+        style={
+          theme
+            ? { color: "black", backgroundColor: "white" }
+            : { color: "white", backgroundColor: "black" }
+        }
+      >
+        Inline Styling
+      </h1>
+
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
 }
 
 export default CSSuse;
