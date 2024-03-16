a3ecdfaec5e6a4e2053a1be11aaeefdcf0e966a0
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-16 14:59:58 -0300

    added forms

--- src/components/inputs/ReactInputs.jsx
+++ src/components/inputs/ReactInputs.jsx
@@ -1,18 +1,57 @@
 import { useState } from "react";
-import './input.css'
+import "./input.css";
 
 function Inputs() {
-   const getInputs = () => {};
+  const [objData, setObjData] = useState({});
+  const getInputs = (value, name) => {
+    let data = { [name]: value };
 
-   return (
-    <div className="form">
-      <input placeholder="Write your name" />
-      <input placeholder="Write your age"  />
-      <input placeholder="Write your hobbies"  />
-      <input placeholder="Write a Date"  />
-      <button>Submit</button>
-    </div>
-   );
+    setObjData({ ...objData, ...data });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(objData);
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <div className="form">
+        <input
+          placeholder="Write your name"
+          name="name"
+          type="text"
+          onChange={(e) => getInputs(e.target.value, e.target.name)}
+        />
+        <input
+          placeholder="Write your age"
+          name="age"
+          type="number"
+          onChange={(e) => getInputs(e.target.value, e.target.name)}
+        />
+        <input
+          placeholder="Write your hobbies"
+          name="hobbies"
+          type="text"
+          onChange={(e) => getInputs(e.target.value, e.target.name)}
+        />
+        <input
+          placeholder="Write a Date"
+          name="date"
+          type="date"
+          onChange={(e) => getInputs(e.target.value, e.target.name)}
+        />
+        <input
+          placeholder="Write a Date"
+          name="date"
+          type="file"
+          onChange={(e) => getInputs(e.target.value, e.target.name)}
+        />
+        <button onClick={submit}>Submit</button>
+        <button type="reset">Reset</button>
+      </div>
+    </form>
+  );
 }
 
-export default Inputs
+export default Inputs;
