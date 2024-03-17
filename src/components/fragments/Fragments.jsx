b2f95a61f84f9a4673f228d58b3b1a6322e3a80e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-17 16:55:49 -0300

    added fragments

---
+++ src/components/fragments/Fragments.jsx
@@ -0,0 +1,16 @@
+function Fragments() {
+  return (
+    <React.Fragment className="fragment">
+      <>
+        <h1>Hello</h1>
+        <h1>World</h1>
+      </>
+      <>
+        <h1>Hello</h1>
+        <h1>World</h1>
+      </>
+    </React.Fragment>
+  );
+}
+
+export default Fragments;
--- src/components/innerhtml/Innerhtml.jsx
+++ src/components/innerhtml/Innerhtml.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import "react-quill/dist/quill.snow.css"
+
 function InnerHTML() {
+  const [quill, setQuill] = useState("");
   // HTML
   let data = `<p style="font-size: 25px; color: red;">This is my name</p>`;
   let data2 = `<b style="font-size: 25px; color: red;">This is my name</bcd s>`;
+
+  const getQuillData = (value) => {
+    setQuill(value)
+  console.log(quill)
   return (
     <div className="InnerHTML">
       <div dangerouslySetInnerHTML={{ __html: data }}></div>
       <div dangerouslySetInnerHTML={{ __html: data2 }}></div>
       {/* JSX */}
       <p style={{ fontSize: 25, color: "red" }}>This is my name</p>
+
+      <ReactQuill onChange={getQuillData}/>
     </div>
   );
 }
 
-export default InnerHTML;
+export default InnerHTML
