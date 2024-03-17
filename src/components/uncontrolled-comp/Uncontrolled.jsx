cc8fcc357a5617463d535f5df6dd4ec78a06a84a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-17 13:48:45 -0300

    added uncontrolled logic

--- src/components/uncontrolled-comp/Uncontrolled.jsx
+++ src/components/uncontrolled-comp/Uncontrolled.jsx
@@ -3,11 +3,26 @@
 function Uncontrolled() {
   let name = React.createRef();
   let age = React.createRef();
+
+  const getName = (e) => {
+    console.log(e.target.value);
+  }
+
+  const submit = () => {
+    console.log(name.current.value);
+    console.log(age.current.age);
+  }
+
   return (
-    <div>
+    <div className="">
+      {/*Uncontrolled Components */}
       <input type="Name" ref={name}/>
       <input type="Age" ref={age}/>
-      <button>Submit</button>
+
+      {/*Controlled Components */}
+      <input type="Name" onChange={getName}/>
+      <input type="Age" onChange={getAge}/>
+      <button onClick={submit}>Submit</button>
     </div>
   )
 }
