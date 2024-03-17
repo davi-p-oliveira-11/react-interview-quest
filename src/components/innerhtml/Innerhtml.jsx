d4063ebf54788e9d97c2bad3cd89f8329bd4fcf7
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-17 14:28:55 -0300

    removed strict mode

--- src/components/innerhtml/Innerhtml.jsx
+++ src/components/innerhtml/Innerhtml.jsx
@@ -1,9 +1,15 @@
 function InnerHTML() {
+  // HTML
   let data = `<p style="font-size: 25px; color: red;">This is my name</p>`;
-  return <div className="InnerHTML">
-    <div dangerouslySetInnerHTML={{__html: data}}></div>
-    <p style={{ fontSize: 25, color: "red"}}>This is my name</p>
-  </div>
+  let data2 = `<b style="font-size: 25px; color: red;">This is my name</bcd s>`;
+  return (
+    <div className="InnerHTML">
+      <div dangerouslySetInnerHTML={{ __html: data }}></div>
+      <div dangerouslySetInnerHTML={{ __html: data2 }}></div>
+      {/* JSX */}
+      <p style={{ fontSize: 25, color: "red" }}>This is my name</p>
+    </div>
+  );
 }
 
-export default InnerHTML
+export default InnerHTML;
